46ec1ae1b96a69e3dbe3bbf5567c583630c167d3
Mubasshir08 <msr4212019@gmail.com>
2026-01-02 14:40:02 +0600

    app.listen code added on server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,15 @@
 import "dotenv/config";
 import mongoDB from "./db/db.js";
+import {app} from "./app.js";
 
 // db code initialize
-mongoDB();
+mongoDB()
+.then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`\nServer is running on port ${process.env.PORT} ✅\n`);
+    });
+})
+.catch((error) => {
+    console.error("MongoDB Connection Failed ❌ : " , error);
+    process.exit(1);
+})
