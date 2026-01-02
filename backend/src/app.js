973562d464844d4c82180b4db55af47a73ed8d0c
Mubasshir08 <msr4212019@gmail.com>
2026-01-02 14:39:26 +0600

    user routes included on app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,4 +13,10 @@ app.use(express.urlencoded({extended : true, limit : "16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// routes import
+import userRoutes from "./routes/user.routes.js";
+
+// rotes declaration
+app.use("/api/v1/users", userRoutes);
+
 export {app};
