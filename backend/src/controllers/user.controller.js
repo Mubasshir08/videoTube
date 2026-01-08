185ff453408f2b60dcb4a6ab4e6780bd13b5a90b
Mubasshir08 <msr4212019@gmail.com>
2026-01-08 22:03:51 +0600

    add code for check if coverImage file exist on user.controller.js

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import {ApiError} from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 import { User } from "../models/user.model.js";
 import { uploadOnClodinary } from "../utils/cloudinary.js";
 
@@ -23,7 +24,10 @@ const registerUser = asyncHandler(async (req, res) => {
 
    if(existedUser) throw new ApiError(409, "User Already Existed");
    const avatarLocalPath = req.files?.avatar[0]?.path;
-   const coverImageLocalPath = req.files?.coverImage[0]?.path;
+   let coverImageLocalPath;
+   if(req.files?.coverImage && req.files?.coverImage[0]?.path){
+      coverImageLocalPath = req.files?.coverImage[0]?.path;
+   }
 
    // console.log(avatarLocalPath, coverImageLocalPath);
    if(!avatarLocalPath) throw new ApiError(400, "Avatar is required");
@@ -34,9 +38,19 @@ const registerUser = asyncHandler(async (req, res) => {
 
    if(!avatar) throw new ApiError(400, "Avatar is required");
 
-   res.status(201).json({
-      message: "ok"
-   })
+   const user = await User.create({
+      fullName,
+      userName,
+      email,
+      password,
+      avatar : avatar.url,
+      coverImage : coverImage?.url || ""
+   });
+
+   if(!user) throw new ApiError(500, "Something went wrong while creating user"); 
+
+
+   res.status(201).json(new ApiResponse(200, user, "User created successfully"));
 });
 
 export {registerUser};
