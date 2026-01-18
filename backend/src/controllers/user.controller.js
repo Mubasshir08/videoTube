684c0b320afba367e8af02e626d930a1c836d127
Mubasshir08 <msr4212019@gmail.com>
2026-01-18 11:52:13 +0600

    add getWatchHistory method in user.controller.js

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -212,7 +212,7 @@ const getUserDetails = asyncHandler(async (req, res) => {
    res
    .status(200)
    .json(new ApiResponse(200, req.user, "User details fetched successfully"));
-})
+});
 
 // get channel details 
 const getUserChannelProfile = asyncHandler(async (req, res) => {
@@ -272,7 +272,56 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
          }
       }
    ])
-})
+});
+
+// get watch history
+const getWatchHistory = asyncHandler(async (req, res) => {
+   const user = await User.aggregate([
+      {
+         $match : {
+            _id : new mongoose.Types.ObjectId(req.user._id)
+         }
+      },
+      {
+         $lookup : {
+            from : "videos",
+            localField : "watchHistory",
+            foreignField : "_id",
+            as : "watchHistory",
+            pipeline : [
+               {
+                  $lookup : {
+                     from : "users",
+                     localField : "owner",
+                     foreignField : "_id",
+                     as : "owner",
+                     pipeline : [
+                        {
+                           $project : {
+                              fullName : 1,
+                              userName : 1,
+                              avatar : 1
+                           }
+                        }
+                     ]
+                  }
+               }
+            ]
+         }
+      },
+      {
+         $addFields : {
+            $owner : {
+               $first : "$owner"
+            }
+         }
+      }
+   ]);
+
+   res
+   .status(200)
+   .json(new ApiResponse(200, user[0].watchHistory, "Watch history fetched successfully"));
+});
 
 export {
   registerUser,
@@ -284,5 +333,6 @@ export {
   updateAvatar,
   updateCoverImage,
   getUserDetails,
-  getUserChannelProfile
+  getUserChannelProfile,
+  getWatchHistory
 };
